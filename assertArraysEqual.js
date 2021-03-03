f0090014e0bc38d676f9273791450383fbcf9975
Mike <mbrie041@gmail.com>
2021-03-03 01:52:56 +0000

    Comparing two arrays and returning a message

---
+++ assertArraysEqual.js
@@ -0,0 +1,14 @@
+
+const assArraysEq = function(actual,expected) {
+  const errMsg = `🛑🛑🛑 Assertation Failed:  ${actual} !== ${expected}`;
+  const corrMsg = `✅✅✅ Assertation Passed: ${actual} === ${expected}`;
+
+  console.assert(JSON.stringify(actual) !== JSON.stringify(expected), corrMsg);
+  console.assert(JSON.stringify(actual) === JSON.stringify(expected), errMsg);
+} 
+
+
+
+//TEST CODE
+assArraysEq([1, 2, 3], [1, 2, 3]) // => should PASS
+assArraysEq([1, 2, 3], [1, 2, "3"]) // => should not PASS

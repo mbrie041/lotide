09b4e77d089f82f287410a17bfab6f625a9d8d39
Mike <mbrie041@gmail.com>
2021-03-08 19:23:53 +0000

    refactoring to make tests modular

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,6 @@
-
-const assertEqual = function(actual,expected) {
-  const errMsg = `🛑🛑🛑 Assertation Failed:  ${actual} !== ${expected}`;
-  const corrMsg = `✅✅✅ Assertation Passed: ${actual} === ${expected}`;
-
-  console.assert(actual !== expected, corrMsg);
-  console.assert(actual === expected, errMsg);
-
-};
 const eqArrays = function(actual,expected) {
   
   return JSON.stringify(actual) === JSON.stringify(expected);
-} 
+};
 
-
-
-//TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;

0a0772f6cb149a8b7590368d983d009369c4aeb1
Mike <mbrie041@gmail.com>
2021-03-08 19:16:40 +0000

    refactoring to make all test files modular

--- tail.js
+++ tail.js
@@ -1,16 +1,7 @@
-
-const assertEqual = function(actual,expected) {
-
-  const errMsg = `🛑🛑🛑 Assertation Failed:  ${actual} !== ${expected}`;
-  const corrMsg = `✅✅✅ Assertation Passed: ${actual} === ${expected}`;
-
-  console.assert(actual !== expected, corrMsg);
-  console.assert(actual === expected, errMsg);
-};
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 const tail = function(array) {
   return array.slice(1);
 };
-//TEST CODE
-tail(words);
-assertEqual(words.length, 3);
+
+
+module.exports = tail;
